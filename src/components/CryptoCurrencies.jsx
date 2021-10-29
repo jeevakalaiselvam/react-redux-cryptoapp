11d70878b4436c2af9a8cdf38a1f688acb7adb49
jeevakalaiselvam <jeevak001@outlook.com>
2021-10-29 22:04:31 +0530

    Exchanges working

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -4,31 +4,44 @@ import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Loader from './Loader';
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState([]);
-  const [searchTerm,setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    
-    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     setCryptos(filteredData);
-  }, [cryptosList,searchTerm]);
+  }, [cryptosList, searchTerm]);
 
-  if (isFetching) return "Loading...";
+  if (isFetching) return <Loader />;
 
   return (
     <>
-      {!simplified && <div className="search-crypto">
-        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)}></Input>
-      </div>}
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          ></Input>
+        </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link key={currency.id} to={`/crypto/${currency.id}`}>
-              <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" alt="" src={currency.iconUrl} />} hoverable>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={
+                  <img className="crypto-image" alt="" src={currency.iconUrl} />
+                }
+                hoverable
+              >
                 <p>Price: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
                 <p>Daily Change: {currency.change}%</p>
--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -19,6 +19,7 @@ import {
   useGetCryptoHistoryQuery,
 } from '../services/cryptoApi';
 import LineChart from './LineChart';
+import Loader from './Loader';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -32,7 +33,7 @@ const CryptoDetails = () => {
     timePeriod,
   });
 
-  if (isFetching) return 'Loading...';
+  if (isFetching) return <Loader />;
   const cryptoDetails = data?.data?.coin;
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
